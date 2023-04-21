b95a95d94a44ec3f68c529b7da6258e4bfc6748e
박지연 <parkjiyeoun@bagjiyeon-ui-MacBookPro.local>
2023-04-21 23:57:40 +0900

    Add: signin 리뷰반영완료

---
+++ src/pages/SignIn/KakaoAuth/KakaoAuth.js
@@ -0,0 +1,63 @@
+import React, { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { PulseLoader } from 'react-spinners';
+import { API } from '../../../config';
+
+function KakaoAuth() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const CODE = location.search.split('=')[1];
+
+  function getKakaoToken() {
+    fetch(`${API.kakaoAuthToken}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+      },
+      body: `grant_type=authorization_code&client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&code=${CODE}`,
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.access_token) {
+          fetch(`${API.kakaoLogin}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json;charset=utf-8' },
+            body: JSON.stringify({
+              kakaoAccessToken: data.access_token,
+            }),
+          })
+            .then(response => response.json())
+            .then(result => {
+              if (result.token) {
+                localStorage.setItem('TOKEN', result.token);
+                alert('로그인 성공했어요!');
+                navigate('/');
+              }
+            });
+        } else {
+          alert('다시 한 번 로그인해주세요!');
+          navigate('/signin');
+        }
+      });
+  }
+
+  useEffect(() => {
+    getKakaoToken();
+  }, []);
+
+  return (
+    <Spinner>
+      <PulseLoader color="#6200EE" margin={8} size={20} />
+    </Spinner>
+  );
+}
+
+export default KakaoAuth;
+
+const Spinner = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,7 +1,106 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled, { css } from 'styled-components';
+import { KAKAO_AUTH_URL } from './KakaoAuth/KakaoAuthUrl';
+import logoBtnImg from '../../assets/images/SignIn/kakaoBtn.png';
+import logo from '../../assets/images/muse-logo-white-3x.png';
+import backgroundImg from '../../assets/images/SignIn/img.png';
 
 function SignIn() {
-  return <div />;
+  const navigate = useNavigate();
+
+  function handleLogin() {
+    window.location.href = KAKAO_AUTH_URL;
+  }
+
+  return (
+    <>
+      <Wrap imgUrl={backgroundImg}>
+        <div />
+      </Wrap>
+      <Container imgUrl={backgroundImg}>
+        <Title
+          onClick={() => {
+            navigate('/');
+          }}
+        />
+        <SubTitle>
+          당신의 <span>뮤즈</span>를 찾으세요.
+        </SubTitle>
+        <SubText>로그인하고 빠른 시작하기</SubText>
+        <KakaoButton imgUrl={logoBtnImg} onClick={handleLogin} />
+      </Container>
+    </>
+  );
 }
 
 export default SignIn;
+
+const Wrap = styled.div`
+  position: relative;
+
+  div {
+    ${prop =>
+      prop.imgUrl &&
+      css`
+        background-image: url(${backgroundImg});
+      `}
+    width: 100%;
+    height: 100vh;
+    background-size: cover;
+  }
+`;
+
+const Container = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  text-align: center;
+  transform: translate(-50%, -10%);
+`;
+
+const Title = styled.div`
+  background-image: url(${logo});
+  background-size: cover;
+  background-position: center center;
+  width: 238px;
+  height: 98px;
+  margin: auto;
+  margin-bottom: 20px;
+`;
+
+const SubTitle = styled.p`
+  margin-bottom: 20px;
+  font-size: 18px;
+  color: #cccccc;
+
+  span {
+    color: #ffffff;
+    font-weight: 700;
+  }
+`;
+
+const SubText = styled.p`
+  display: inline-block;
+  margin-top: 140px;
+  margin-bottom: 16px;
+  border: 2px solid #cccccc;
+  border-radius: 40px 40px 40px 0px;
+  padding: 10px 16px;
+  font-size: 14px;
+  color: #ffffff;
+`;
+
+const KakaoButton = styled.div`
+  ${prop =>
+    prop.imgUrl &&
+    css`
+      background-image: url(${logoBtnImg});
+    `}
+  background-size: cover;
+  background-position: center center;
+  width: 400px;
+  height: 60px;
+  border-radius: 12px;
+  cursor: pointer;
+`;
